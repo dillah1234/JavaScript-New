c8bca7cbdad0ffc2cf22c58aae4ebc49aa88db5d
dillah1234 <arrafif0722@abudzar.sch.id>
2024-07-30 11:10:22 +0700

    praktek baru

---
+++ variabel.js
@@ -0,0 +1,20 @@
+var b;
+console.log((b));
+// soal nomer 1
+
+var name = "rapip";
+console.log(name);
+var umur = "umur 16 tahun";
+console.log(umur);
+var live = "tinggal di depok";
+console.log(live);   
+console.log(name+' ' +umur + ' ' + live)
+// soal nomer 2
+var a = null
+console.log(a);
+// soal nomer 3
+const alas = 5
+const tinggi = 7
+const LuasSegitiga = 0.5 * alas * tinggi
+console.log("luas segitiga adalah = " + LuasSegitiga);
+// soal nomer 4
