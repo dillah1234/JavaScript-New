e2483c46516373e7db7ec0ca9075a9a31ec49c83
dillah1234 <arrafif0722@abudzar.sch.id>
2024-08-05 13:17:09 +0700

    js4

---
+++ m4/tugas4pt2.js
@@ -0,0 +1,15 @@
+var pendaftaran = false;
+var pembayaran = false;
+
+if (pendaftaran === true) {
+  if (pembayaran === true) {
+    console.log("Dapat mengikuti Kursus");
+  } else {
+    console.log("Tidak dapat mengikuti Kursus");
+  }
+} else {
+  console.log("Tidak dapat mengikuti Kursus");
+}
+
+// if buat kondisi 
+   
